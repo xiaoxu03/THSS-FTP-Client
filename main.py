7e9070b1ca839871428061bd5ba58eb12e1dfdbc
xiaoxu03 <1371507762@qq.com>
2023-10-20 15:24:27 +0800

    added retr post and pasv todo: stor and seprate thread for command and data

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import requests
+import re
+
 
 from enum import Enum
 
@@ -36,6 +39,11 @@ class Client:
         self.ui.tableFile.setColumnCount(5)
         self.ui.tableFile.setHorizontalHeaderLabels(['文件名', '修改时间', '大小', '所有者', '状态'])
         self.ui.tableFile.setSelectionBehavior(1)
+        self.ui.tableFile.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.tableFile.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.ui.tableFile.verticalHeader().hide()
+        self.ui.tableFile.setShowGrid(False)
+        self.ui.tableFile.verticalHeader().setDefaultSectionSize(12)
 
     def send_command(self, data):
         self.ui.textCommand.insertPlainText("Client: " + data)
@@ -58,49 +66,96 @@ class Client:
         self.mode = Mode.PASV
 
     def port(self):
-        self.send_command('PORT 127,0,0,1,200,0\r\n')
+        self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        res = requests.get('https://myip.ipip.net', timeout=5).text
+        print(res)
+        self.data_ip = re.findall(r"\d+.\d+.\d+.\d+", res)[0]
+        self.data_socket.bind((self.data_ip, 0))
+        self.data_ip = self.data_socket.getsockname()[0]
+        self.data_port = self.data_socket.getsockname()[1]
+        self.data_socket.listen(1)
+        self.send_command('PORT ' + self.data_ip.replace('.', ',') + ',' + str(int(self.data_port / 256)) +
+                          ',' + str(self.data_port % 256) + '\r\n')
         self.receive_response()
+        self.mode = Mode.PORT
 
     def list(self):
+        file_info = []
+        all_data = ''
+
         if self.mode == Mode.NONE:
             msg_box = QMessageBox(QMessageBox.Warning, '警告', '请指定连接模式为PASV或PORT')
             msg_box.exec_()
             return
         elif self.mode == Mode.PASV:
-            self.data_socket = socket.socket()
-            self.data_socket.connect((self.data_ip, self.data_port))
             self.send_command('LIST\r\n')
-            server_response = self.receive_response()
-            if server_response[0] == '5':
-                msg_box = QMessageBox(QMessageBox.Warning, '警告', '无法连接到服务器')
-                msg_box.exec_()
-                return
+            self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.data_socket.connect((self.data_ip, self.data_port))
+        elif self.mode == Mode.PORT:
+            self.send_command('LIST\r\n')
+            self.data_socket, addr = self.data_socket.accept()
+
+        self.ui.tableFile.clearContents()
+
+        server_response = self.receive_response()
+        if server_response[0] == '5':
+            msg_box = QMessageBox(QMessageBox.Warning, '警告', '无法连接到服务器')
+            msg_box.exec_()
+            return
+
+        while True:
+            data = self.data_socket.recv(1024).decode()
+            if not data:
+                break
+            all_data += data
+
+        self.data_socket.close()
+        self.mode = Mode.NONE
+
+        all_data = all_data.split('\r\n')[0:-1]
+        for fdata in all_data:
+            info = fdata.split(' ')
+            info = [s for s in info if s != '']
+            status = info[0]
+            owner = info[2]
+            size = info[4]
+            edit_time = info[5] + ' ' + info[6] + ' ' + info[7]
+            name = info[8]
+            file_info.append([name, edit_time, size, owner, status])
+
+        for i in range(0, len(file_info)):
+            self.ui.tableFile.insertRow(i)
+            for j in range(0, len(file_info[i])):
+                self.ui.tableFile.setItem(i, j, QTableWidgetItem(file_info[i][j]))
+
+    def retr(self, filename):
+        filedir = self.directory
+        if filedir[-1] != '/':
+            filedir += '/'
+        filedir += filename
+        if self.mode == Mode.NONE:
+            msg_box = QMessageBox(QMessageBox.Warning, '警告', '请指定连接模式为PASV或PORT')
+            msg_box.exec_()
+            return
+        elif self.mode == Mode.PASV:
+            self.send_command('RETR ' + filedir + '\r\n')
+            self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.data_socket.connect((self.data_ip, self.data_port))
+        elif self.mode == Mode.PORT:
+            self.send_command('RETR ' + filedir + '\r\n')
+            self.data_socket, addr = self.data_socket.accept()
+            self.receive_response()
+
+        with open(filename, 'wb') as f:
             while True:
-                data = self.data_socket.recv(1024).decode()
+                data = self.data_socket.recv(1024)
                 if not data:
                     break
-                info = data.split(' ')
-                status = info[0]
-                owner = info[2]
-                size = info[4]
-                edit_time = info[5] + ' ' + info[6] + ' ' + info[7]
-                name = info[8]
-                item_1 = QTableWidgetItem(name)
-                item_2 = QTableWidgetItem(edit_time)
-                item_3 = QTableWidgetItem(size)
-                item_4 = QTableWidgetItem(owner)
-                item_5 = QTableWidgetItem(status)
-                self.ui.tableFile.insertRow(0)
-                self.ui.tableFile.setItem(0, 0, item_1)
-                self.ui.tableFile.setItem(0, 1, item_2)
-                self.ui.tableFile.setItem(0, 2, item_3)
-                self.ui.tableFile.setItem(0, 3, item_4)
-                self.ui.tableFile.setItem(0, 4, item_5)
+                f.write(data)
 
-            self.data_socket.close()
-            self.mode = Mode.NONE
-        elif self.mode == Mode.PORT:
-            pass
+        self.data_socket.close()
+        self.receive_response()
+        self.mode = Mode.NONE
 
     def receive_response(self):
         data = self.control_socket.recv(1024).decode()
@@ -131,7 +186,7 @@ class Client:
             msg_box.exec_()
             return
 
-        self.control_socket = socket.socket()
+        self.control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.control_socket.connect((ip, int(port)))
 
         self.connect_ip = ip
@@ -151,10 +206,13 @@ class Client:
             msg_box = QMessageBox(QMessageBox.Warning, '警告', '密码不是一个合法的邮箱')
             msg_box.exec_()
             return
-
-        self.pasv()
+        self.port()
+        # self.pasv()
 
         self.list()
+
+        self.pasv()
+        self.retr('2.txt')
 
         user_command = 'QUIT' + '\r\n'
         self.send_command(user_command)
